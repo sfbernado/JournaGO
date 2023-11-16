8773a7617c0a49a1e513e30f79ebec7c46f2a2dc
sfbernado <stanislausfb@student.ub.ac.id>
2023-11-17 02:20:49 +0700

    Update SplashActivity.kt

--- app/src/main/java/com/example/journalapp/SplashActivity.kt
+++ app/src/main/java/com/example/journalapp/SplashActivity.kt
@@ -4,15 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this,
-                MainActivity::class.java)))
+                MainActivity::class.java))
             finish()
         }, 2000)
     }
